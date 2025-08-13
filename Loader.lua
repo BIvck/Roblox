9be73b462e61e32d2e912e86f820c50ca5109e73
BIvck <118535712+BIvck@users.noreply.github.com>
2025-08-13 20:26:58 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/af0e28187d9a45218b9656ebc2009718"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/ac1a08ef942a4f789c5ca048d1f17f51"))()

bab2fbfaaf727343d8705d4f3a1cf3415389ca14
BIvck <118535712+BIvck@users.noreply.github.com>
2025-09-20 21:06:39 +0800

    Update and rename mangomustartds.lua to GameTables.lua

---
+++ GameTables.lua
@@ -0,0 +1,31 @@
+local Updates = {
+    South_Bronx = {
+        Status = {
+            Version = "1.0.7",
+            PlaceVersion = 3190,
+            VcPlaceVersion = 684,
+            Build = "Free", -- Free, Gay, Eboy, Nigger
+            Released = true
+        }
+    },
+    A_Typical_Neighborhood = {
+        Status = "1.0.1",
+        PlaceVersion = 0,
+        Build = "Free", -- Free, Gay, Eboy, Nigger
+        Released = false
+    },
+    Philly_Streetz = {
+        Status = "1.0.1",
+        PlaceVersion = 0,
+        Build = "Free", -- Free, Gay, Eboy, Nigger
+        Released = false
+    },
+    Tha_Bronx_3 = {
+        Status = "1.0.1",
+        PlaceVersion = 0,
+        Build = "Free", -- Free, Gay, Eboy, Nigger
+        Released = false
+    },
+}
+
+return Updates
